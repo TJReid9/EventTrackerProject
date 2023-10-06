0b51c3fa85059c55a0760046e30d8f965a2f4fca
TJReid9 <tjreid20@icloud.com>
2023-10-06 16:44:16 -0600

    Configure JPA project, stub and test jamSession entity

---
+++ JPAJamSession/src/main/java/com/skilldistillery/xtreme/entities/jamSession.java
@@ -0,0 +1,66 @@
+package com.skilldistillery.xtreme.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+
+@Table(name="jam_session")
+public class jamSession {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	
+	//TODO Add other properties
+
+	public jamSession() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		jamSession other = (jamSession) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "jamSession [id=" + id + ", title=" + title + "]";
+	}
+
+}
---
+++ JPAJamSession/src/test/java/com/skilldistillery/xtreme/entities/jamSessionTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.xtreme.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class jamSessionTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private jamSession jam;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAJamSession");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		jam = em.find(jamSession.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		jam = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(jam);
+		assertEquals("Jam at The Alley", jam.getTitle());
+	}
+
+}
